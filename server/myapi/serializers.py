9e35a7633e051105fea2a0b66a38e704dc0e5f5d
Sergey Agaka <sergeyagaka@gmail.com>
2022-03-12 08:05:09 +0300

    API was made

--- server/myapi/serializers.py
+++ server/myapi/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 
-from .models import Material
+from .models import Material, Marks, Company
 
 class MaterialSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Material
         fields = ('material_name', 'image_url')
+
+class MarksSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Marks
+        fields = ('material_id', 'mark_name', 'image_url', 'text')
+
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('company_name', 'phone', 'web_link', 'eco', 'materials', 'adress', 'longitude', 'latitude', 'hours')
