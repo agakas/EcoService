ad05dbe12ab822bf29d4f60b9a5954bd13ea00fe
Sergey Agaka <sergeyagaka@gmail.com>
2022-03-12 08:53:36 +0300

    fix APIs

--- server/myapi/serializers.py
+++ server/myapi/serializers.py
@@ -1,3 +1,5 @@
+import requests
+
 from rest_framework import serializers
 
 from .models import Material, Marks, Company
@@ -8,11 +10,13 @@ class MaterialSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('material_name', 'image_url')
 
 class MarksSerializer(serializers.HyperlinkedModelSerializer):
+    material_id = MaterialSerializer(many=False)
     class Meta:
         model = Marks
         fields = ('material_id', 'mark_name', 'image_url', 'text')
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    materials = MaterialSerializer(many=True)
     class Meta:
         model = Company
         fields = ('company_name', 'phone', 'web_link', 'eco', 'materials', 'adress', 'longitude', 'latitude', 'hours')
